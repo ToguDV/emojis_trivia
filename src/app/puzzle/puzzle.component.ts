f7e5b3de578a97f4753ed336c58f8832681c3a95
Togu <timeofgetup@gmail.com>
2024-04-06 19:45:26 -0500

    add animation on change emojis

--- src/app/puzzle/puzzle.component.ts
+++ src/app/puzzle/puzzle.component.ts
@@ -2,25 +2,36 @@ import { Component} from '@angular/core';
 import { TriviaEmojisService } from '../utils/trivia-emojis.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+const enterTransition = transition(':enter', [
+  style({ opacity: 0 }),
+  animate('1s ease-in', style({ opacity: 1 }))
+]
+);
+const fadeIn = trigger('fadeIn', [enterTransition]);
 
 @Component({
   selector: 'app-puzzle',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './puzzle.component.html',
-  styleUrl: './puzzle.component.css'
+  styleUrl: './puzzle.component.css',
+  animations: [fadeIn]
 })
 export class PuzzleComponent  {
 
   
   constructor(private triviaEmojis: TriviaEmojisService) {}
   emojis: string = "";
-  trivia: string[] = ["", ""];
+  trivia: string[] = ["", "", ""];
   isAnswered: boolean = false;
+  description: string = "";
   input: string = "";
-  answer: string = ''
+  answer: string = '';
   data: any;
+
+  isShown = false;
 
   ngOnInit(): void {
     
@@ -43,28 +54,35 @@ export class PuzzleComponent  {
     
     `);
 
-    this.onReload();
+    this.randomEmoji();
     
+  }
+
+  fadeInOut(): void {
+    this.isShown = !this.isShown;
   }
 
   onAnswer(): void {
     this.isAnswered = true;
-    this.answer = this.triviaEmojis.getAnswer(this.trivia[1]);
+    this.answer = this.trivia[1];
   }
 
   onInput(event: Event): void {
     this.input = (event.target as HTMLInputElement).value;
   }
 
-  async onReload() : Promise<void> {
+  async randomEmoji() : Promise<void> {
+    
     this.triviaEmojis.getData().subscribe(data => {
       this.data = data;
       const random = Math.floor(Math.random() * this.data.length);
-      this.trivia = [this.data[random].emojis, this.data[random].title];
+      this.trivia = [this.data[random].emojis, this.data[random].title, this.data[random].description];
       this.emojis = this.trivia[0];
+      this.description = this.trivia[2];
     });
     this.isAnswered = false;
     this.input = "";
+    this.fadeInOut();
   }
 
 
--- src/app/utils/trivia-emojis.service.ts
+++ src/app/utils/trivia-emojis.service.ts
@@ -8,27 +8,6 @@ export class TriviaEmojisService {
 
   constructor(private http: HttpClient) { }
 
-  async getRandomTrivia() {
-
-    let result = ["", ""];
-    try {
-      const response = await fetch('http://158.69.122.84:25568/api/Trivia');
-      const data = await response.json();
-      const random = Math.floor(Math.random() * data.length);
-      result = [data[random].emojis, data[random].title];
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      result = ["error", "error"];
-    }
-
-    return result;
-  }
-
-  getAnswer(id: string) {
-    return id;
-  }
-
-
   getData() {
     return this.http.get('http://158.69.122.84:25568/api/Trivia');
   }
