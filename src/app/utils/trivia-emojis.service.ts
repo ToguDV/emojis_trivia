653db10af95b18a0af18326e2cd4208d2218921e
Andrey Villegas <45081449+ToguDV@users.noreply.github.com>
2025-04-02 01:18:36 -0500

    Filter SSR api calls

--- src/app/utils/trivia-emojis.service.ts
+++ src/app/utils/trivia-emojis.service.ts
@@ -6,9 +6,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TriviaEmojisService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object // Detecta si es servidor o navegador
+  ) { }
 
-  getData() {
-    return this.http.get('https://emojisapi.caprover.togudv.com/api/Trivia');
+  getData(): Observable<any> {
+    if (isPlatformBrowser(this.platformId)) {
+      // Solo hace la petición si está en el navegador
+      return this.http.get('https://emojisapi.caprover.togudv.com/api/Trivia');
+    } else {
+      // Si está en el servidor, retorna un Observable vacío
+      return of(null);
+    }
   }
 }
